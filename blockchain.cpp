53a7c0b4c72570dcca9d15e807c0890954ee64b1
NJLin <765980891@qq.com>
2019-04-16 15:57:09 +0800

    query contract trx modify

--- blockchain.cpp
+++ blockchain.cpp
@@ -731,6 +731,7 @@ void GDW::collectContractTransactions(QString contractAddress)
         int toBlockHeight = currentBlockHeight;
 
         if( fromBlockHeight > toBlockHeight)    return;
+        if(toBlockHeight > fromBlockHeight + 100)   toBlockHeight = fromBlockHeight + 2000;
         postRPC( toJsonFormat( "id_blockchain_list_contract_transaction_history+" + contractAddress + "+" + QString::number(toBlockHeight),
                                "blockchain_list_contract_transaction_history",
                                QStringList() << QString::number(fromBlockHeight) << QString::number(toBlockHeight)
